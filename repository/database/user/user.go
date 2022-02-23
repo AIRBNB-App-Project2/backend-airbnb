158f9c6f1dd202d47ff4e6bde7b0db9ec83c4e66
faliqadlan <faliq_adlan@student.ub.ac.id>
2022-02-23 13:54:40 +0700

    edit get byid resp

--- repository/database/user/user.go
+++ repository/database/user/user.go
@@ -3,7 +3,6 @@ package user
 import (
 	"be/entities"
 	"be/utils"
-	"fmt"
 
 	"github.com/lithammer/shortuuid"
 	"gorm.io/gorm"
@@ -42,60 +41,60 @@ func (repo *UserDb) Create(user entities.User) (entities.User, error) {
 	return user, nil
 }
 
-func (repo *UserDb) GetById(uid string) (, error) {
-	user := entities.User{}
+func (repo *UserDb) GetById(uid string) (GetByIdResponse, error) {
+	user := GetByIdResponse{}
 
-	if err := repo.db.Where("user_uid =?", uid).First(&user); err != nil {
+	if err := repo.db.Model(&entities.User{}).Where("user_uid =?", uid).First(&user); err != nil {
 		return user, err.Error
 	}
 
 	return user, nil
 }
 
-func (repo *UserDb) Update(user_uid string, upUser entities.User) (entities.Room, error) {
-	tx := repo.db.Begin()
+// func (repo *UserDb) Update(user_uid string, upUser entities.User) (entities.Room, error) {
+// 	tx := repo.db.Begin()
 
-	defer func() {
-		if r := recover(); r != nil {
-			tx.Rollback()
-		}
-	}()
+// 	defer func() {
+// 		if r := recover(); r != nil {
+// 			tx.Rollback()
+// 		}
+// 	}()
 
-	if err := tx.Error; err != nil {
-		return entities.Room{}, err
-	}
+// 	if err := tx.Error; err != nil {
+// 		return entities.Room{}, err
+// 	}
 
-	resRoom1 := entities.Room{}
+// 	resRoom1 := entities.Room{}
 
-	if err := tx.Model(&entities.Room{}).Where("room_uid = ?", room_uid).Find(&resRoom1).Error; err != nil {
-		tx.Rollback()
-		return entities.Room{}, err
-	}
+// 	if err := tx.Model(&entities.Room{}).Where("room_uid = ?", room_uid).Find(&resRoom1).Error; err != nil {
+// 		tx.Rollback()
+// 		return entities.Room{}, err
+// 	}
 
-	if resRoom1.User_uid != user_uid {
-		tx.Rollback()
-		return entities.Room{}, errors.New(gorm.ErrInvalidData.Error())
-	}
+// 	if resRoom1.User_uid != user_uid {
+// 		tx.Rollback()
+// 		return entities.Room{}, errors.New(gorm.ErrInvalidData.Error())
+// 	}
 
-	if res := tx.Model(&entities.Room{}).Where("room_uid = ?", room_uid).Delete(&resRoom1); res.RowsAffected == 0 {
-		log.Info(res.RowsAffected)
-		tx.Rollback()
-		return entities.Room{}, errors.New(gorm.ErrRecordNotFound.Error())
-	}
-	resRoom1.DeletedAt = gorm.DeletedAt{}
-	resRoom1.ID = 0
-	if res := tx.Create(&resRoom1); res.Error != nil {
-		tx.Rollback()
-		return entities.Room{}, res.Error
-	}
+// 	if res := tx.Model(&entities.Room{}).Where("room_uid = ?", room_uid).Delete(&resRoom1); res.RowsAffected == 0 {
+// 		log.Info(res.RowsAffected)
+// 		tx.Rollback()
+// 		return entities.Room{}, errors.New(gorm.ErrRecordNotFound.Error())
+// 	}
+// 	resRoom1.DeletedAt = gorm.DeletedAt{}
+// 	resRoom1.ID = 0
+// 	if res := tx.Create(&resRoom1); res.Error != nil {
+// 		tx.Rollback()
+// 		return entities.Room{}, res.Error
+// 	}
 
-	if res := tx.Model(&entities.Room{}).Where("room_uid = ?", room_uid).Updates(entities.Room{Name: upRoom.Name, Category: upRoom.Category, Price: upRoom.Price, Detail: upRoom.Detail}); res.Error != nil {
-		tx.Rollback()
-		return entities.Room{}, res.Error
-	}
+// 	if res := tx.Model(&entities.Room{}).Where("room_uid = ?", room_uid).Updates(entities.Room{Name: upRoom.Name, Category: upRoom.Category, Price: upRoom.Price, Detail: upRoom.Detail}); res.Error != nil {
+// 		tx.Rollback()
+// 		return entities.Room{}, res.Error
+// 	}
 
-	return resRoom1, tx.Commit().Error
-}
+// 	return resRoom1, tx.Commit().Error
+// }
 
 func (repo *UserDb) Delete(userUid string) error {
 
