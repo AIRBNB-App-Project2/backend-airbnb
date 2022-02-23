938683c01e24054c8aefd9c2b4ce74ccdaa796bb
faliqadlan <faliq_adlan@student.ub.ac.id>
2022-02-23 22:18:59 +0700

    update default image

--- entities/image.go
+++ entities/image.go
@@ -5,5 +5,5 @@ import "gorm.io/gorm"
 type Image struct {
 	gorm.Model
 	Room_uid string
-	Url      string `gorm:"default:'https://www.teralogistics.com/wp-content/uploads/2020/12/default.png'"`
+	Url      string `gorm:"default:'https://test-upload-s3-rogerdev.s3.ap-southeast-1.amazonaws.com/6216503718eb9324b8213a1f.png'"`
 }
